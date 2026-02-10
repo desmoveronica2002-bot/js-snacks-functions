95dbbc405c4d00f4bc24c444832c10b2d0f39610
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-02-10 17:02:08 +0100

    finish es3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function iniziali (stringhe, lettera){
+    let newArray =[];
+
+    for(let i=0; i<stringhe.length; i++){
+        if(stringhe[i][0]===lettera){
+            newArray.push(stringhe[i]);
+        }
+    }
+
+    return newArray;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(iniziali(names, 'A'))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
